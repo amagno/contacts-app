ce167d463076068cb9ffd438cd0325077305944f
alexandre <amagnomonteiro@gmail.com>
2019-03-21 13:06:21 -0300

    more tests

--- ContactsApp.EndToEndTests/cypress/integration/contacts-app.spec.js
+++ ContactsApp.EndToEndTests/cypress/integration/contacts-app.spec.js
@@ -14,25 +14,40 @@ context("Contacts App", () => {
       .first()
       .should("have.text", "Contacts APP")
   })
+  it('Show contacts list', () => {
+    cy.get('.contact-card').should('have.length.greaterThan', 1)
+
+  })
 
   it('Test create new contact', () => {
     cy.server()
     cy.route('POST', '**/api/contacts/').as('createContact')
-    
-    cy.get('input[name="FirstName"]').type(faker.name.firstName())
-    cy.get('input[name="LastName"]').type(faker.name.lastName())
-    cy.get('input[name="Email"]').type(faker.internet.email())
-    cy.get('input[name="Birthday"]').type('1991-01-01')
-    cy.get('input[name="Company"]').type(faker.company.companyName())
-    cy.get('input[name="Address"]').type(faker.address.secondaryAddress())
-    cy.get('input[name="Avatar"]').type(faker.internet.avatar())
-    cy.get('input[name="Phone"]').type(faker.phone.phoneNumber("(##) #####-####"))
+    let count = 0
+    cy.get('.contact-card').each(e => {
+      count++
+    }).then(() => {
+      cy.get('#ToggleCreateContactForm').click()
+      cy.get('input[name="FirstName"]').type(faker.name.firstName())
+      cy.get('input[name="LastName"]').type(faker.name.lastName())
+      cy.get('input[name="Email"]').type(faker.internet.email())
+      cy.get('input[name="Birthday"]').type('1991-01-01')
+      cy.get('input[name="Company"]').type(faker.company.companyName())
+      cy.get('input[name="Address"]').type(faker.address.secondaryAddress())
+      cy.get('input[name="Avatar"]').type(faker.internet.avatar())
+      cy.get('input[name="Phone"]').type(faker.phone.phoneNumber("(##) #####-####"))
 
-    cy.get('button[type="submit"]').click()
+      cy.get('button[type="submit"]').click()
 
-    cy.wait('@createContact').then(xhr => {
-      assert.equal(201, xhr.status)
+      cy.wait('@createContact').then(xhr => {
+        assert.equal(201, xhr.status)
+      })
+
+      cy.get('.contact-card').should('have.length.greaterThan', count)
     })
+    
+
+
+  
 
   })
 })
--- ContactsApp.UI/src/index.js
+++ ContactsApp.UI/src/index.js
@@ -4,6 +4,25 @@ import axios from 'axios'
 
 const apiUrl = 'https://localhost:5001/api/contacts/'
 const form = document.getElementById("CreateContactForm")
+const container = document.getElementById('CreateContactFormContainer')
+const button = document.getElementById('ToggleCreateContactForm')
+
+let showCreateContactForm = false;
+
+container.style.display = 'none';
+
+
+const toggleCreateContactForm = () => {
+  showCreateContactForm = !showCreateContactForm;
+
+  container.style.display = showCreateContactForm ? 'block' : 'none'
+  button.innerText = !showCreateContactForm ? 'Create Contact' : 'Hide'
+}
+
+button.onclick = event => {
+  event.preventDefault()
+  toggleCreateContactForm()
+}
 
 form.onsubmit = async event => {
   event.preventDefault()
@@ -11,12 +30,58 @@ form.onsubmit = async event => {
   const formData = new FormData(event.target)
 
   const object = {};
-  formData.forEach(function(value, key){
+  formData.forEach((value, key) => {
       object[key] = value;
   });
 
   const response = await axios.post(apiUrl, object)
 
-  console.log(response);
+  if (response.status !== 201)
+  {
+    alert('Error on create contact')
+  }
 
+  renderContacts()
+  toggleCreateContactForm()
 }
+
+const createContactHtml = contact => `
+<div class="contact-card">
+  <div class="contact-card-field">
+    ${contact.firstName} ${contact.lastName}
+  </div>
+  <div class="contact-card-field">
+    ${contact.email}
+  </div>
+  <div class="contact-card-field">
+    ${contact.birthday}
+  </div>
+  <div class="contact-card-field">
+  </div>
+  <div class="contact-card-info">
+    <div class="contact-card-field">
+    </div>
+    <div class="contact-card-field">
+    </div>
+    <div class="contact-card-field">
+    </div>
+    <div class="contact-card-field">
+    </div>
+
+  </div>
+</div>
+`.trim()
+
+
+const renderContacts = async () => {
+  const response = await axios.get(apiUrl)
+  const contacts = response.data
+  let html = ''
+  contacts.forEach(contact => {
+    html += createContactHtml(contact)
+  })
+
+  document.getElementById('ContactsList').innerHTML = html
+}
+
+renderContacts()
